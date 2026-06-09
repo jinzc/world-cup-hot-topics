01b1c5147516fa970193e5eb514e09dc40831271
北境之王卡哇伊 <a.jinzichen@gmail.com>
2026-06-09 09:52:00 +0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,46 +2,45 @@ import json
 import ssl
 import urllib.request
 
-# 世界杯关键词库（中英文覆盖）
+# 世界杯关键词库（严格围绕世界杯，移除易误匹配的国家名单个词）
 WORLD_CUP_KEYWORDS = [
+    # 中文核心词（必须包含这些才认为是世界杯相关）
     "世界杯", "世预赛", "美加墨", "国足", "中国男足", "国足世预赛",
     "中国vs", "vs中国", "出线", "晋级", "淘汰", "小组赛", "淘汰赛",
     "1/8决赛", "1/4决赛", "半决赛", "决赛", "冠军", "金球奖", "金靴奖",
     "VAR", "点球", "红牌", "黄牌", "越位", "进球", "绝杀", "扳平",
     "逆转", "爆冷", "黑马", "死亡之组", "射手榜", "积分榜", "赛程",
     "直播", "转播", "解说", "预测", "竞猜", "帽子戏法", "梅开二度",
-    "伤停补时", "加时赛", "点球大战",
+    "伤停补时", "加时赛", "点球大战", "揭幕战", "开幕", "闭幕式",
+    "大力神杯", "足联", "FIFA", "国际足联",
+    # 球星（知名世界杯球星，避免普通球员名误匹配）
     "梅西", "姆巴佩", "C罗", "内马尔", "哈兰德", "贝林厄姆", "维尼修斯",
     "德布劳内", "莱万", "凯恩", "孙兴慜", "三笘薰", "久保建英",
-    "阿根廷", "巴西", "德国", "法国", "西班牙", "英格兰", "意大利",
-    "荷兰", "葡萄牙", "比利时", "克罗地亚", "摩洛哥", "日本", "韩国",
-    "伊朗", "沙特", "澳大利亚", "加拿大", "墨西哥", "美国", "哥伦比亚",
-    "乌拉圭", "智利", "厄瓜多尔", "秘鲁", "巴拉圭", "玻利维亚", "委内瑞拉",
-    "洪都拉斯", "哥斯达黎加", "巴拿马", "牙买加", "海地", "危地马拉",
-    "萨尔瓦多", "尼加拉瓜", "多米尼加", "古巴", "波多黎各", "特立尼达",
-    "巴巴多斯", "格林纳达", "圣卢西亚", "圣文森特", "安提瓜", "多米尼克",
-    "圣基茨", "巴哈马", "百慕大", "开曼", "特克斯", "凯科斯", "安圭拉",
-    "蒙特塞拉特", "瓜德罗普", "马提尼克", "法属圭亚那", "圣马丁",
-    "荷属圣马丁", "圣巴泰勒米", "圣尤斯特歇斯", "萨巴", "博奈尔",
-    "库拉索", "阿鲁巴",
-    "world cup", "worldcup", "wc2026", "fifa", "group stage", "knockout",
+    "莫德里奇", "亚马尔", "拉什福德", "萨卡", "贝林", "恩德里克",
+    "佩德里", "加维", "罗德里", "范戴克", "萨拉赫", "卢卡库",
+    "格列兹曼", "登贝莱", "萨利巴", "赖斯", "贝林厄姆",
+    "马丁内斯", "大马丁", "迪马利亚", "阿尔瓦雷斯", "恩佐",
+    "琼阿梅尼", "卡马文加", "孔德", "于帕", "科纳特",
+    "穆西亚拉", "维尔茨", "哈弗茨", "基米希", "诺伊尔",
+    "罗德里戈", "米利唐", "马尔基尼奥斯", "阿利松", "埃德森",
+    "奥塔门迪", "罗梅罗", "麦卡利斯特", "帕雷德斯", "德保罗",
+    "B费", "B席", "菲利克斯", "莱奥", "努诺", "门德斯",
+    "孙兴慜", "李刚仁", "金玟哉", "黄喜灿",
+    "久保建英", "三笘薰", "富安健洋", "远藤航",
+    "塔雷米", "阿兹蒙", "贾汉巴赫什",
+    # 英文核心词
+    "world cup", "worldcup", "wc2026", "fifa world cup", "group stage", "knockout",
     "round of 16", "quarter final", "semi final", "final", "champion",
     "golden ball", "golden boot", "var", "penalty", "red card", "yellow card",
     "offside", "goal", "hat trick", "brace", "injury time", "extra time",
-    "penalty shootout",
-    "messi", "mbappe", "neymar", "ronaldo", "haaland", "bellingham", "vinicius",
+    "penalty shootout", "opening match", "opening ceremony", "closing ceremony",
+    # 英文球星
+    "messi", "mbappe", "mbappé", "neymar", "ronaldo", "haaland", "bellingham", "vinicius",
     "de bruyne", "lewandowski", "kane", "son heung-min", "mitoma", "kubo",
-    "argentina", "brazil", "germany", "france", "spain", "england", "italy",
-    "netherlands", "portugal", "belgium", "croatia", "morocco", "japan",
-    "korea", "iran", "saudi", "australia", "canada", "mexico", "usa",
-    "colombia", "uruguay", "chile", "ecuador", "peru", "paraguay", "bolivia",
-    "venezuela", "honduras", "costa rica", "panama", "jamaica", "haiti",
-    "guatemala", "el salvador", "nicaragua", "dominican", "cuba", "puerto rico",
-    "trinidad", "barbados", "grenada", "saint lucia", "saint vincent",
-    "antigua", "dominica", "saint kitts", "bahamas", "bermuda", "cayman",
-    "turks", "caicos", "anguilla", "montserrat", "guadeloupe", "martinique",
-    "french guiana", "saint martin", "sint maarten", "saint barthelemy",
-    "sint eustatius", "saba", "bonaire", "curacao", "aruba",
+    "modric", "lamine yamal", "rashford", "saka", "endrick",
+    "pedri", "gavi", "rodri", "van dijk", "salah", "lukaku",
+    "griezmann", "dembele", "saliba", "rice", "musiala",
+    "wirtz", "havertz", "kimmich", "neuer", "alisson", "ederson",
 ]
 
 
